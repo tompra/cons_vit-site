f5df1559fc70426c57cc6827f4ea11c7971e3c3d
tompra <tomprayon@gmail.com>
2024-12-03 16:36:32 +0100

    import open modal for functionality

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,11 @@
-interface ButtonProps {
-    text: string;
-}
+import { ButtonProps } from '../utils/props';
+import { useModal } from '../context/context';
 
 const Button: React.FC<ButtonProps> = ({ text }) => {
+    const { openModal } = useModal();
+
     return (
-        <button type='button' className='btn btn-primary'>
+        <button type='button' className='btn btn-primary' onClick={openModal}>
             {text}
         </button>
     );
